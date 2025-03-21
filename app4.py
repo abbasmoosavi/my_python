1b31a2112224e18938ade417adcc24c8281f090d
moosaviabbas93@gmail.com <moosaviabbas93@gmail.com>
2025-03-21 12:58:22 +0330

    How to generating random values

--- app4.py
+++ app4.py
@@ -5,3 +5,22 @@ ecommerce.shipping.calc_shipping()
 # Second way
 from ecommerce.shipping import calc_shipping
 calc_shipping()
+
+import random
+
+for i in range(4):
+    print(random.random())
+    print(random.randint(20,30))
+    
+team = ['Abbas', 'Arvin', 'Moosavi']
+leader= random.choice(team)
+print(leader)
+
+class Dice:
+    def roll(self):
+        first = random.randint(1,6)
+        second= random.randint(1,6)
+        return first, second
+    
+dice = Dice()
+print(dice.roll())
